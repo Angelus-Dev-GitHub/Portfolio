ef0a513c8868825e02e44e4cd9c4ea9a8b9249c5
Angelus-Dev-GitHub <t.dufosse666@gmail.com>
2021-02-04 11:52:18 +0100

    fill about page

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,6 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\ContentManagementSystemRepository;
+use App\Repository\DesignPatternRepository;
+use App\Repository\FrameworkRepository;
+use App\Repository\LanguageRepository;
+use App\Repository\LibrairieRepository;
+use App\Repository\MethodRepository;
+use App\Repository\PictureRepository;
+use App\Repository\SoftwareRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +20,32 @@ class AboutController extends AbstractController
     /**
      * @Route("/about", name="about")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository,
+                          LanguageRepository $languageRepository,
+                          LibrairieRepository $librairieRepository,
+                          FrameworkRepository $frameworkRepository,
+                          DesignPatternRepository $designPatternRepository,
+                          SoftwareRepository $softwareRepository,
+                          MethodRepository $methodRepository,
+                          ContentManagementSystemRepository $contentManagementSystemRepository,
+                          PictureRepository $pictureRepository): Response
     {
+        $user = $userRepository->findOneBy(['lastname' => 'DUFOSSE']);
+
+        $cv = $pictureRepository->findOneBy(['name' => 'Thierry_DUFOSSE_CV_DeveloppeurWeb.pdf']);
+
+
         return $this->render('about/index.html.twig', [
-            'controller_name' => 'AboutController',
+            'user' => $user,
+            'cv' => $cv,
+            'languages' => $languageRepository->findAll(),
+            'librairies' => $librairieRepository->findAll(),
+            'frameworks' => $frameworkRepository->findAll(),
+            'designPatterns' => $designPatternRepository->findAll(),
+            'softwares' => $softwareRepository->findAll(),
+            'methods' => $methodRepository->findAll(),
+            'contentManagementSystems' => $contentManagementSystemRepository->findAll(),
+
         ]);
     }
 }
